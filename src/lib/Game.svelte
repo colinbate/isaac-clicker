03c78b2fe2da0910e79057e1898ebd9bbb0d928e
Colin Bate <colin@colinbate.com>
2023-06-20 12:37:42 -0300

    Fix mobile

--- src/lib/Game.svelte
+++ src/lib/Game.svelte
@@ -13,15 +13,15 @@
   
 </script>
 <div class="overflow-hidden rounded-lg bg-white dark:bg-gray-900 shadow text-left">
-  <div class="grid grid-cols-1 md:grid-cols-5 gap-4 px-4 py-5 sm:p-6 h-[36rem]">
+  <div class="grid grid-cols-1 md:grid-cols-5 gap-4 px-4 py-5 sm:p-6 md:h-[36rem]">
     <div class="md:col-span-5 bg-gray-200 dark:bg-gray-800 rounded-md p-2 flex items-center justify-between">
       <div>Score: {fmt.format($score)}</div>
       <button type="button" class="px-4 py-1 rounded bg-svelte" on:click={resetScore}>Reset</button>
     </div>
-    <div class="md:col-span-3 flex flex-col flex-1 overflow-y-auto">
+    <div class="md:col-span-3 flex flex-col flex-1 md:overflow-y-auto">
       <Board on:click={doClick} />
     </div>
-    <div class="md:col-span-2 flex flex-col flex-1 overflow-y-auto">
+    <div class="md:col-span-2 flex flex-col flex-1 md:overflow-y-auto">
       <Store {store}/>
     </div>
   </div>
